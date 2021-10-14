20c044fd96c541432521d1ac9288e0e1e3b9e024
TROUVERIE Joachim <joachim.trouverie@linoame.fr>
2021-10-14 17:32:06 +0200

    chg: Manage flask_sqlalchemy wrapped methods

--- pylint_flask_sqlalchemy/__init__.py
+++ pylint_flask_sqlalchemy/__init__.py
@@ -3,11 +3,24 @@
 from astroid import MANAGER, ClassDef
 from pylint.lint import PyLinter
 
+# flask-sqlalchemy wraps some sqlalchemy objects as its own
+# see https://github.com/pallets/flask-sqlalchemy/blob/main/src/flask_sqlalchemy/__init__.py#L104
+FLASK_SQLALCHEMY_WRAPS = [
+    "relationship",
+    "relation",
+    "dynamic_loader",
+]
+
 VERSION = "0.2.0"
 
 
 def register(linter: PyLinter):
     """Plugin registration."""
+
+
+def sort_module_keys(key: str):
+    return -1 if key in FLASK_SQLALCHEMY_WRAPS else 1
+
 
 def transform(cls) -> None:
     """Mimics Flask-SQLAlchemy's _include_sqlalchemy ."""
@@ -16,12 +29,20 @@ def transform(cls) -> None:
         import sqlalchemy.orm
 
         for module in sqlalchemy, sqlalchemy.orm:
-            for key in module.__all__:
-                cls.locals[key] = [ClassDef(key, None)]
+            for key in sorted(module.__all__, key=sort_module_keys):
+                if key not in FLASK_SQLALCHEMY_WRAPS:
+                    cls.locals[key] = [ClassDef(key, None)]
+                else:
+                    cls.locals[key] = [
+                        ClassDef(key, None),
+                        cls.locals["Query"]
+                    ]
+
     if cls.name == "scoped_session":
         from sqlalchemy.orm import Session
 
         for key in Session.public_methods:
             cls.locals[key] = [ClassDef(key, None)]
+
 
 MANAGER.register_transform(ClassDef, transform)
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -9,9 +9,9 @@ E2 = ("E1101", "Instance of 'SQLAlchemy' has no 'Integer' member")
 E3 = ("E1101", "Instance of 'SQLAlchemy' has no 'String' member")
 E4 = ("E1101", "Instance of 'scoped_session' has no 'add' member")
 E5 = ("E1101", "Instance of 'scoped_session' has no 'commit' member")
+E6 = ("E1101", "Instance of 'relationship' has no 'filter' member")
 
 
-@pytest.mark.new
 @pytest.mark.parametrize("with_plugin", [True, False])
 @pytest.mark.parametrize(
     "filename, errors",
@@ -36,3 +36,28 @@ def test_plugin(with_plugin, filename, errors):
             if err_id in message and err_msg in message
         ]
         assert bool(messages) != with_plugin
+
+
+@pytest.mark.parametrize(
+    "filename, errors",
+    [
+        ("sqlalchemy_relationship", [E6])
+    ],
+)
+def test_error_with_flask_sqlalchemy_wraps(filename, errors):
+    """These tests raises errors only when using the plugin."""
+    args = [
+        f"tests/data/{filename}.py",
+        "--load-plugins",
+        "pylint_flask_sqlalchemy",
+    ]
+    pylint_stdout, _ = lint.py_run(" ".join(args), return_std=True)
+    err_messages = pylint_stdout.readlines()
+
+    for error in errors:
+        err_id, err_msg = error
+        messages = [
+            message for message in err_messages
+            if err_id in message and err_msg in message
+        ]
+        assert len(messages) == 0
